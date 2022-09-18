caac3821a14a6c787b64929f0bbab387882ec8c1
Jade Ferguson <jadesnider2440@gmail.com>
2022-09-17 20:04:19 -0700

    Set up cards & filter js, structured rows/columns

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@
 
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap";
+import Cards from "./Components/Cards/Cards";
+import Filters from "./Components/Filters/Filters";
 
 function App() {
   return (
@@ -9,6 +11,21 @@ function App() {
       <h1 className="text-center ubuntu my-4">
         Rick & Morty <span className="text-primary">WiKi</span>
       </h1>
+
+      <div className="container">
+        <div className="row">
+          <div className="col-3">
+            <Filters></Filters>
+          </div>  
+          <div className="col-8">
+            <div className="row">
+              <Cards></Cards>
+              <Cards></Cards>
+              <Cards></Cards>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/Components/Filters/Filters.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+// raface is a short cut for below, did this because we installed ES7
+// react extension
+const Filters = () => {
+  return (
+    <div>
+      Filters
+    </div>
+  )
+}
+
+export default Filters
